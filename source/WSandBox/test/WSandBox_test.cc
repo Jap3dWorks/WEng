3b9e78fca9adb27780adc57a0e2ee88cfbb72bbc
jordi.amposta <jap3dworks@gmail.com>
2023-05-05 01:49:00 +0200

    UPDATE source CMakes, Add test example

---
+++ source/WSandBox/test/WSandBox_test.cc
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HelloTest, BasicAssertions2) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(TestSecondFamily, TestAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
