21096856a46c951617007fdec5674d219fea8b3c
jordi.amposta <jap3dworks@gmail.com>
2024-01-02 19:36:49 +0100

    WIP Save Work

--- Source/WVulkanTest/Source/WVulkanTest.cpp
+++ Source/WVulkanTest/Source/WVulkanTest.cpp
@@ -50,9 +50,9 @@ struct Vertex{
 };
 
 const std::vector<Vertex> Vertices = {
-    {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-    {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-    {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+    {{0.0f, -0.5f}, {1.0f, 1.0f, 0.0f}},
+    {{0.5f, 0.5f}, {0.0f, 1.0f, 1.0f}},
+    {{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}
 };
 
 namespace VulkanUtils
