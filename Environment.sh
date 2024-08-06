4c2c6b48d1b6abc23c0fb856718b9dd650e0d82a
jordi <jordi@localhost.localdomain>
2024-08-06 14:10:58 +0200

    Update requirements to be compatible with helix, Fix tiniobj find cmake

--- Environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:./Scripts
-export PYTHONPATH=$PYTHONPATH:./Python
-
-echo "Set environment for wcli"
-
--- cmake/Findtinyobjloader.cmake
+++ cmake/Findtinyobjloader.cmake
@@ -1,24 +1,35 @@
+set(tinyobjloader_FOUND FALSE)
+
 IF(UNIX)
 
-    # find_package(tinyobjloader REQUIRED)
-    find_path(tinyobjloader_INCLUDE_DIRS tiny_obj_loader.h)
+    find_path(
+        tinyobjloader_INCLUDE_DIRS 
+        NAMES "tiny_obj_loader.h"
+    )
 
-    if(tinyobjloader_INCLUDE_DIRS)
-
-        message(STATUS "tinyobjloader found")
+    if(
+        NOT "${tinyobjloader_INCLUDE_DIRS}" STREQUAL "tinyobjloader_INCLUDE_DIRS-NOTFOUND" AND
+        EXISTS "${tinyobjloader_INCLUDE_DIRS}/tiny_obj_loader.h"
+    )
+        message(STATUS "tiny_obj_loader.h found at: ${tinyobjloader_INCLUDE_DIRS}")
         set(tinyobjloader_FOUND TRUE)
     
     else()
 
         set(tinyobjloader_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/../tinyobjloader)
+
         # check if tiniobjloader directory exists
         IF(NOT EXISTS ${tinyobjloader_INCLUDE_DIRS})
-            message(FATAL_ERROR "tinyobjloader directory not found at ${tinyobjloader_INCLUDE_DIRS}")
+            message(FATAL_ERROR "tiny_obj_loader.h directory not found at ${tinyobjloader_INCLUDE_DIRS}")
         ELSE()
-            message(STATUS "tinyobjloader directory found at ${tinyobjloader_INCLUDE_DIRS}")
+            message(STATUS "tiny_obj_loader directory found at ${tinyobjloader_INCLUDE_DIRS}")
             set(tinyobjloader_FOUND TRUE)
         ENDIF()
 
     ENDif()
+
+ELSE()
+
+    message(FATAL_ERROR "Not Valid Platform.")
 
 ENDIF()
