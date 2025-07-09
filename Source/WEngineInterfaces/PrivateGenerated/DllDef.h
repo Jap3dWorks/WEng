26a1e5394142541387293d9e6288101a38802b8b
Jordi Amposta <Jordi Amposta>
2025-07-09 18:55:46 +0200

    WIP

---
+++ Source/WEngineInterfaces/PrivateGenerated/DllDef.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#if defined(_WIN32)
+    #ifdef WENGINEINTERFACES_EXPORTS
+        #define WENGINEINTERFACES_API __declspec(dllexport)
+    #else
+        #define WENGINEINTERFACES_API __declspec(dllimport)
+    #endif
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #ifdef WENGINEINTERFACES_EXPORTS
+        #define WENGINEINTERFACES_API __attribute__((visibility("default")))
+    #else
+        #define WENGINEINTERFACES_API
+    #endif
+#else
+    #define WENGINEINTERFACES_API
+#endif
+
+#if defined(_WIN32)
+    #ifdef WENGINE_EXPORTS
+        #define WENGINE_API __declspec(dllexport)
+    #else
+        #define WENGINE_API __declspec(dllimport)
+    #endif
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #ifdef WENGINE_EXPORTS
+        #define WENGINE_API __attribute__((visibility("default")))
+    #else
+        #define WENGINE_API
+    #endif
+#else
+    #define WENGINE_API
+#endif
+
+#if defined(_WIN32)
+    #ifdef WCORE_EXPORTS
+        #define WCORE_API __declspec(dllexport)
+    #else
+        #define WCORE_API __declspec(dllimport)
+    #endif
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #ifdef WCORE_EXPORTS
+        #define WCORE_API __attribute__((visibility("default")))
+    #else
+        #define WCORE_API
+    #endif
+#else
+    #define WCORE_API
+#endif
+
+#if defined(_WIN32)
+    #ifdef WIMPORTERS_EXPORTS
+        #define WIMPORTERS_API __declspec(dllexport)
+    #else
+        #define WIMPORTERS_API __declspec(dllimport)
+    #endif
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #ifdef WIMPORTERS_EXPORTS
+        #define WIMPORTERS_API __attribute__((visibility("default")))
+    #else
+        #define WIMPORTERS_API
+    #endif
+#else
+    #define WIMPORTERS_API
+#endif
+
+#if defined(_WIN32)
+    #ifdef WRENDER_EXPORTS
+        #define WRENDER_API __declspec(dllexport)
+    #else
+        #define WRENDER_API __declspec(dllimport)
+    #endif
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #ifdef WRENDER_EXPORTS
+        #define WRENDER_API __attribute__((visibility("default")))
+    #else
+        #define WRENDER_API
+    #endif
+#else
+    #define WRENDER_API
+#endif
