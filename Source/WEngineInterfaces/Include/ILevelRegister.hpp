cc0679c34a65182bdfe6519204d3c5aeda01666d
Jordi Amposta <Jordi Amposta>
2025-07-18 20:41:18 +0200

    WLEVEL wip LevelRegister

--- Source/WEngineInterfaces/Include/ILevelRegister.hpp
+++ Source/WEngineInterfaces/Include/ILevelRegister.hpp
@@ -8,4 +8,6 @@ public:
 
     virtual ~ILevelRegister() = default;
 
+    
+
 };
--- Source/WLevel/Include/WLevelRegister/WLevelRegister.hpp
+++ Source/WLevel/Include/WLevelRegister/WLevelRegister.hpp
@@ -3,17 +3,19 @@
 #include "WCore/WCore.hpp"
 #include "ILevelRegister.hpp"
 #include "WCore/WIdPool.hpp"
+#include "WCore/TFunction.hpp"
 
 #include <unordered_map>
 #include <memory>
 
 /**
- * @brief 
+ * @brief Register Open and Close Levels.
  */
 class WLEVEL_API WLevelRegister: public ILevelRegister {
 public:
 
     WLevelRegister() = default;
+
     ~WLevelRegister() override = default;
 
     WId RegisterLevel(std::unique_ptr<ILevelRegister> && in_level) {
@@ -22,6 +24,16 @@ public:
 
         return id;
     }
+
+    void LoadLevel(const WId & in_id) {}
+
+    void CloseCurrent() {}
+
+    WId Current();
+
+    void ApplyCurrent(const TFunction<void(ILevelRegister*)> & fn);
+
+    void Apply(const WId & in_id, const TFunction<void(ILevelRegister*)> & fn);
 
 private:
     
