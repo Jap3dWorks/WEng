a9849cefce8b970f1b48200a169264bc92f78cd7
jordi <wowtio@gmail.com>
2026-02-07 17:47:54 +0100

    EMACS ide issues

--- .dir-locals.el
+++ .dir-locals.el
@@ -20,43 +20,44 @@
                                  (ja-execute-shell-script (concat (project-root (project-current)) "Scripts/cmd-launch-spacers.sh"))))
 
                    ;; (setenv "LD_LIBRARY_PATH" "lib")
+                   (with-eval-after-load 'dap-mode
+                       (progn
+                         (dap-register-debug-template
+                          "[LLDB][WVulkanTest] Run"
+                          (list :type "lldb-vscode"
+                                :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
+                                :request "launch"
+                                :program "bin/WVulkanTest" 
+                                :name "LLDB::Run"
+                                :env '(("LD_LIBRARY_PATH" . "lib"))))
 
-                   (dap-register-debug-template
-                    "[LLDB][WVulkanTest] Run"
-                    (list :type "lldb-vscode"
-                          :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
-                          :request "launch"
-                          :program "bin/WVulkanTest" 
-                          :name "LLDB::Run"
-                          :env '(("LD_LIBRARY_PATH" . "lib"))))
+                         (dap-register-debug-template
+                          "[LLDB][WSpacers] Run"
+                          (list :type "lldb-vscode"
+                                :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
+                                :request "launch"
+                                :program "bin/WSpacers" 
+                                :name "[LLDB][WSpacers]"
+                                :env '(("LD_LIBRARY_PATH" . "lib"))))
 
-                   (dap-register-debug-template
-                    "[LLDB][WSpacers] Run"
-                    (list :type "lldb-vscode"
-                          :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
-                          :request "launch"
-                          :program "bin/WSpacers" 
-                          :name "[LLDB][WSpacers]"
-                          :env '(("LD_LIBRARY_PATH" . "lib"))))
+                         (dap-register-debug-template
+                          "[LLDB][unittest] WEngineObjects"
+                          (list :type "lldb-vscode"
+                                :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
+                                :request "launch"
+                                :program "bin/WEngineObjects_unittest" 
+                                :name "LLDB::Run"
+                                :env '(("LD_LIBRARY_PATH" . "lib"))))
 
-                   (dap-register-debug-template
-                    "[LLDB][unittest] WEngineObjects"
-                    (list :type "lldb-vscode"
-                          :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
-                          :request "launch"
-                          :program "bin/WEngineObjects_unittest" 
-                          :name "LLDB::Run"
-                          :env '(("LD_LIBRARY_PATH" . "lib"))))
-
-                   (dap-register-debug-template
-                    "[LLDB][unittest] WCore"
-                    (list :type "lldb-vscode"
-                          :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
-                          :request "launch"
-                          :program "bin/WCore_unittest" 
-                          :name "LLDB::Run"
-                          :env '(("LD_LIBRARY_PATH" . "lib"))))
-
+                         (dap-register-debug-template
+                          "[LLDB][unittest] WCore"
+                          (list :type "lldb-vscode"
+                                :cwd "${workspaceFolder}/Install/Linux_x86_64_Debug_Standalone"
+                                :request "launch"
+                                :program "bin/WCore_unittest" 
+                                :name "LLDB::Run"
+                                :env '(("LD_LIBRARY_PATH" . "lib"))))
+                         ))
 
 		           )))))
 
