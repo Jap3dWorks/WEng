32a72cc16fe21be2df6eab29c07092c69c68f65d
jordi <wowtio@gmail.com>
2026-03-08 13:36:45 +0100

    WENGINEOBJECTS point light component

--- Source/WEngineObjects/Include/WComponents/WPointLightComponent.hpp
+++ Source/WEngineObjects/Include/WComponents/WPointLightComponent.hpp
@@ -2,6 +2,14 @@
 
 #include "WEngineObjects/WComponent.hpp"
 
+#include <glm/glm.hpp>
+
 class WENGINEOBJECTS_API WPointLightComponent : public WComponent {
-    
+public:
+
+private:
+    bool active{true};
+    float intensity{1.f};
+    glm::vec4 color{0.5, 0.5, 0.5, 1.f};
+    float radius{10.f};
 };
